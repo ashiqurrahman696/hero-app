4dee17d80080741b9457d9705bfd7e184660d047
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-08 07:18:02 +0600

    created banner section

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        Component: MainLayout
+        Component: MainLayout,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+        ],
     },
 ]);
 
