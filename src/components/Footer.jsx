86dccfdb31e1953f4c6d8cc971c7225cf3667c71
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-08 11:35:54 +0600

    created footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import { BsTwitterX } from 'react-icons/bs';
+import logo from '../assets/logo.png';
+import { Link } from 'react-router';
+import { FaFacebook, FaLinkedinIn } from 'react-icons/fa6';
+
+const Footer = () => {
+    return (
+        <footer className="bg-[#001931] text-white">
+            <div className="footer justify-between sm:footer-horizontal  p-10">
+                <aside className="flex items-center">
+                    <img src={logo} alt="Logo" className="w-10" />
+                    <p className="font-semibold">HERO.IO</p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Social links</h6>
+                    <div className="flex gap-4">
+                        <div className="bg-white text-black rounded-full p-1">
+                            <Link to="https://x.com">
+                                <BsTwitterX />
+                            </Link>
+                        </div>
+                        <div className="bg-white text-black rounded-full p-1">
+                            <Link to="https://linkedin.com">
+                                <FaLinkedinIn />
+                            </Link>
+                        </div>
+                        <div className="text-white">
+                            <Link to="https://facebook.com">
+                                <FaFacebook size={22} />
+                            </Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <hr className="max-w-7xl mx-auto text-[#2e4256]" />
+            <div className="footer sm:footer-horizontal footer-center p-10">
+                <aside>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+                </aside>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
     return (
         <div className="font-inter">
             <Navbar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };
