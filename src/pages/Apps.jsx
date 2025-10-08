d2fdcada040122a3757d35d59775f7b603e6de8d
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-08 12:50:31 +0600

    made stats ans search section responsive for mobile devices

--- src/pages/Apps.jsx
+++ src/pages/Apps.jsx
@@ -8,7 +8,7 @@ const Apps = () => {
             <div className="w-full max-w-7xl mx-auto">
                 <h2 className="text-5xl font-bold text-center mb-3">Our All Applications</h2>
                 <p className="text-center text-[#627382] mb-10">Explore All Apps on the Market developed by us. We code for Millions</p>
-                <div className="flex justify-between items-center mb-10">
+                <div className="flex max-sm:flex-col justify-between items-center gap-3 mb-10">
                     <h4 className="text-2xl font-semibold">({apps.length}) Apps Found</h4>
                     <label className="input bg-transparent">
                         <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
