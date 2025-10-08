bbbebc6a5b04aac4936c05a523ccc1318394431d
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-08 19:40:44 +0600

    removed data variable (constant) from app detail page

--- src/pages/AppDetail.jsx
+++ src/pages/AppDetail.jsx
@@ -11,50 +11,6 @@ const AppDetail = () => {
     const singleApp = apps.find(app => app.id === Number(appId));
     if(loading) return <p>Loading...</p>
     const {image, title, companyName, description, size, reviews, ratingAvg, downloads, ratings} = singleApp;
-    const data = [
-        {
-            name: 'Page A',
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: 'Page B',
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
-        },
-        {
-            name: 'Page C',
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
-        },
-        {
-            name: 'Page D',
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: 'Page E',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: 'Page F',
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: 'Page G',
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
-    ];
     return (
         <div className="bg-neutral-100 px-4 py-10 text-[#001931]">
             <div className="w-full max-w-7xl mx-auto">
