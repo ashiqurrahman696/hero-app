d91819f964535e25a9e03e65bd62ef77cc714c5d
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-09 11:14:36 +0600

    made banner heading smaller for small devices

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
     return (
         <div className="text-center bg-neutral-100 px-4 pt-10">
             <div className="max-w-7xl mx-auto space-y-6">
-                <h1 className="text-7xl font-bold flex flex-col text-[#192f45]"><span>We Build</span><span><span className="bg-linear-to-br from-[#632ee3] to-[#9f62f2] text-transparent bg-clip-text">Productive</span> Apps</span></h1>
+                <h1 className="text-5xl sm:text-7xl font-bold flex flex-col text-[#192f45]"><span>We Build</span><span><span className="bg-linear-to-br from-[#632ee3] to-[#9f62f2] text-transparent bg-clip-text">Productive</span> Apps</span></h1>
                 <p className="text-[#627382]">At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting. Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
                 <div className="flex gap-3 justify-center flex-wrap">
                     <Link to="https://play.google.com/" target="_blank" className="btn text-[#001931] bg-transparent border-[#d2d2d2]">
