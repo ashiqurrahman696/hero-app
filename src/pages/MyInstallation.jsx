e864d2d82d2d02ddf246d507e4c60a33535094b5
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-09 08:47:36 +0600

    implemented sort by downloads functionality

--- src/pages/MyInstallation.jsx
+++ src/pages/MyInstallation.jsx
@@ -1,6 +1,3 @@
-import { LuDownload } from "react-icons/lu";
-import demo1 from "../assets/demo-app (1).webp";
-import { FaStar } from "react-icons/fa";
 import { getInstalledApps } from "../utils/localStorage";
 import useApps from "../hooks/useApps";
 import { useEffect, useState } from "react";
@@ -9,11 +6,22 @@ import InstalledCard from "../components/InstalledCard";
 const MyInstallation = () => {
     const {apps, loading, error} = useApps();
     const [installedApps, setInstalledApps] = useState([]);
+    const [sortOrder, setSortOrder] = useState("none")
     useEffect(() => {
         const installedStorage = getInstalledApps();
         const filtered = apps.filter(app => installedStorage.includes(app.id));
         setInstalledApps(filtered);
     }, [apps]);
+    useEffect(() => {
+        let sorted = [...installedApps];
+        if(sortOrder === "high-low"){
+            sorted.sort((a, b) => b.downloads - a.downloads)
+        }
+        if(sortOrder === "low-high"){
+            sorted.sort((a, b) => a.downloads - b.downloads)
+        }
+        setInstalledApps(sorted);
+    }, [sortOrder]);
     return (
         <div className="bg-neutral-100 px-4 py-10 text-[#001931]">
             <div className="w-full max-w-7xl mx-auto">
@@ -21,10 +29,10 @@ const MyInstallation = () => {
                 <p className="text-center text-[#627382] mb-10">Explore All Trending Apps on the Market developed by us</p>
                 <div className="flex max-sm:flex-col justify-between items-center gap-3 mb-10">
                     <h4 className="text-2xl font-semibold">{installedApps.length} Apps Found</h4>
-                    <select className="select bg-neutral-100">
-                        <option>Sort by Download Count</option>
-                        <option>High-Low</option>
-                        <option>Low-High</option>
+                    <select className="select bg-neutral-100" onChange={(e) => setSortOrder(e.target.value)}>
+                        <option value="none" disabled selected>Sort by Download Count</option>
+                        <option value="high-low">High-Low</option>
+                        <option value="low-high">Low-High</option>
                     </select>
                 </div>
                 <div className="space-y-5">
