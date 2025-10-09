ec1bc7a68102fae9c35309ec944a43a0951b4c3a
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-09 08:34:05 +0600

    displayed installed apps on installation page

---
+++ src/components/InstalledCard.jsx
@@ -0,0 +1,30 @@
+import { FaStar } from "react-icons/fa6";
+import { LuDownload } from "react-icons/lu";
+
+const InstalledCard = ({installed}) => {
+    const {image, title, downloads, size, ratingAvg} = installed;
+    return (
+        <div className="bg-base-100 p-4 rounded flex max-sm:flex-col items-center gap-4">
+            <img src={image} alt={title} className="w-20 rounded" />
+            <div className="flex justify-between items-center max-sm:flex-col gap-4 w-full">
+                <div>
+                    <h6 className="text-lg mb-3">{title}</h6>
+                    <div className="flex gap-4 flex-wrap">
+                        <div className="flex items-center gap-1 text-[#00d390] ">
+                            <LuDownload />
+                            <span>{downloads >= 1000000 ? `${downloads / 1000000}M` : `${downloads / 1000}K`}</span>
+                        </div>
+                        <div className="flex items-center gap-1 text-[#ff8811] ">
+                            <FaStar />
+                            <span>{ratingAvg}</span>
+                        </div>
+                        <p className="text-[#627382]">{size} MB</p>
+                    </div>
+                </div>
+                <button className="btn bg-[#00d390] text-white rounded-md">Uninstall</button>
+            </div>
+        </div>
+    );
+};
+
+export default InstalledCard;
--- src/pages/MyInstallation.jsx
+++ src/pages/MyInstallation.jsx
@@ -1,15 +1,26 @@
 import { LuDownload } from "react-icons/lu";
 import demo1 from "../assets/demo-app (1).webp";
 import { FaStar } from "react-icons/fa";
+import { getInstalledApps } from "../utils/localStorage";
+import useApps from "../hooks/useApps";
+import { useEffect, useState } from "react";
+import InstalledCard from "../components/InstalledCard";
 
 const MyInstallation = () => {
+    const {apps, loading, error} = useApps();
+    const [installedApps, setInstalledApps] = useState([]);
+    useEffect(() => {
+        const installedStorage = getInstalledApps();
+        const filtered = apps.filter(app => installedStorage.includes(app.id));
+        setInstalledApps(filtered);
+    }, [apps]);
     return (
         <div className="bg-neutral-100 px-4 py-10 text-[#001931]">
             <div className="w-full max-w-7xl mx-auto">
                 <h2 className="text-5xl font-bold text-center mb-3">Your Installed Apps</h2>
                 <p className="text-center text-[#627382] mb-10">Explore All Trending Apps on the Market developed by us</p>
                 <div className="flex max-sm:flex-col justify-between items-center gap-3 mb-10">
-                    <h4 className="text-2xl font-semibold">1 Apps Found</h4>
+                    <h4 className="text-2xl font-semibold">{installedApps.length} Apps Found</h4>
                     <select className="select bg-neutral-100">
                         <option>Sort by Download Count</option>
                         <option>High-Low</option>
@@ -17,26 +28,7 @@ const MyInstallation = () => {
                     </select>
                 </div>
                 <div className="space-y-5">
-                    <div className="bg-base-100 p-4 rounded flex max-sm:flex-col items-center gap-4">
-                        <img src={demo1} alt="Demo app" className="w-20 rounded" />
-                        <div className="flex justify-between items-center max-sm:flex-col gap-4 w-full">
-                            <div>
-                                <h6 className="text-lg mb-3">Pomocat - Cute Pomodoro Timer</h6>
-                                <div className="flex gap-4 flex-wrap">
-                                    <div className="flex items-center gap-1 text-[#00d390] ">
-                                        <LuDownload />
-                                        <span>9M</span>
-                                    </div>
-                                    <div className="flex items-center gap-1 text-[#ff8811] ">
-                                        <FaStar />
-                                        <span>5</span>
-                                    </div>
-                                    <p className="text-[#627382]">258 MB</p>
-                                </div>
-                            </div>
-                            <button className="btn bg-[#00d390] text-white rounded-md">Uninstall</button>
-                        </div>
-                    </div>
+                    {installedApps.map(installed => <InstalledCard key={installed.id} installed={installed} />)}
                 </div>
             </div>
         </div>
