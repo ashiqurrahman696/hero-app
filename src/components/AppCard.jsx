33177c62e8d7e21fda501ade845b786c79f4e625
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-09 12:01:48 +0600

    added gap between icon and stats text (both download and review) at the bottom of app title in app card

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -15,11 +15,11 @@ const AppCard = ({app}) => {
             <div className="card-body text-center pt-2">
                 <h2 className="card-title">{title}</h2>
                 <div className="flex justify-between w-full">
-                    <div className="flex items-center bg-[#f1f5e8] text-[#00d390] py-1 px-2 rounded">
+                    <div className="flex items-center bg-[#f1f5e8] text-[#00d390] py-1 px-2 rounded gap-1.5">
                         <LuDownload />
                         <span>{downloads >= 1000000 ? `${downloads/1000000}M` : `${downloads/1000}K`}</span>
                     </div>
-                    <div className="flex items-center bg-[#fff0e1] text-[#ff8811] py-1 px-2 rounded">
+                    <div className="flex items-center bg-[#fff0e1] text-[#ff8811] py-1 px-2 rounded gap-1.5">
                         <FaStar />
                         <span>{ratingAvg}</span>
                     </div>
