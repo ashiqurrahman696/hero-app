fb79fe0e7959f25a03610109c807b015df5b6769
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-10-09 16:46:29 +0600

    updated buttons in not found pages

--- src/components/NotFoundBtns.jsx
+++ src/components/NotFoundBtns.jsx
@@ -1,14 +1,12 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 const NotFoundBtns = () => {
+    const navigate = useNavigate();
     return (
-        <div className="flex justify-center gap-4 flex-wrap">
-            <Link to="/" className="btn text-white bg-linear-to-br from-[#632ee3] to-[#9f62f2] rounded-md px-8 py-6">
-                <span className="text-lg">Go to Home</span>
-            </Link>
-            <Link to="/apps" className="btn btn-outline text-violet-700 border border-y-violet-700 hover:bg-linear-to-br hover:from-[#632ee3] hover:to-[#9f62f2] hover:text-white hover:border-0 rounded-md px-8 py-6">
-                <span className="text-lg">Browse Apps</span>
-            </Link>
+        <div className="text-center">
+            <button onClick={() => navigate(-1)} className="btn text-white bg-linear-to-br from-[#632ee3] to-[#9f62f2] rounded-md px-8 py-6">
+                <span className="text-lg">Go Back!</span>
+            </button>
         </div>
     );
 };
